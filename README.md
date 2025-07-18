a80e19359348e24bafa0ce2cb0e66c01f4c69c3d
amey_deshmukh <ameydeshmukh1990@gmail.com>
2025-07-18 15:00:25 +0530

    First commit with README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+This application is a E-Commerce Event System which will handle -
+1. Order Creation
+2. Process Payment
+3. Process Shipment
+4. Inventory Update
+5. Notification System
---
+++ src/main/java/com/ecommerce/ECommerceApp1.java
@@ -0,0 +1,12 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApp1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceApp1.class);
+    }
+}
