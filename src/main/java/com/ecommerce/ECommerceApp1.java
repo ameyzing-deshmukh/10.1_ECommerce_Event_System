0e223f5712e90c7ca4b3480dc794afd41633aa1d
amey_deshmukh <ameydeshmukh1990@gmail.com>
2025-07-21 19:41:19 +0530

    Inventory service - Order placed listener created.

--- src/main/java/com/ecommerce/ECommerceApp1.java
+++ src/main/java/com/ecommerce/ECommerceApp1.java
@@ -1,4 +1,4 @@
-package com.ecommerce.orderservice;
+package com.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
