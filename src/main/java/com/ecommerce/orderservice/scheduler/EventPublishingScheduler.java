0b4971f7a2cd023f8499cfbce807596e503c6311
amey_deshmukh <ameydeshmukh1990@gmail.com>
2025-07-21 14:57:22 +0530

    added scheduler to pick and push 'order-created' events to Kafka. Kafka setup follows.

---
+++ src/main/java/com/ecommerce/orderservice/scheduler/EventPublishingScheduler.java
@@ -0,0 +1,41 @@
+package com.ecommerce.orderservice.scheduler;
+
+import com.ecommerce.orderservice.entity.OutboxEventEntity;
+import com.ecommerce.orderservice.repository.OutboxEventRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.List;
+
+@Slf4j
+@Configuration
+public class EventPublishingScheduler {
+
+    @Autowired
+    private OutboxEventRepository outboxEventRepository;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Scheduled(fixedRate = 20000)
+    @Transactional
+    public void publishEvent(){
+        List<OutboxEventEntity> eventList =  outboxEventRepository.findTop10ByIsProcessedFalseOrderByCreatedAtAsc();
+
+        try {
+            if(eventList.size()>0){
+                log.info(objectMapper.writeValueAsString(eventList));
+            }else
+                log.info("No entry yet");
+        } catch (JsonProcessingException e) {
+
+           log.info(e.getMessage());
+        }
+
+    }
+}
